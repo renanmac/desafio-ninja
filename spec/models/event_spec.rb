f317c1e23a28e709ef609e9c3c716e0985a14e7c
Renan Machado <renanmacs@gmail.com>
2022-01-22 14:32:49 -0300

    Add event spec context

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -13,73 +13,86 @@ RSpec.describe Event, type: :model do
     described_class.new(
       start_at: start_at,
       end_at: end_at,
+      owner_email: 'ninja@mail.com',
       schedule_id: schedules(:getninjas).id,
       room_id: rooms(:subzero).id
     )
   end
 
-  it 'Should not create events in the past' do
-    start_at = 1.day.ago
-    end_at = start_at + 1.hour
-    event = build_event(start_at, end_at)
+  context 'When save fails' do
+    it 'Should not create events in the past' do
+      start_at = 1.day.ago
+      end_at = start_at + 1.hour
+      event = build_event(start_at, end_at)
 
-    expect(event.save).to be false
-    expect(event.errors.messages[:in_the_past].first).to eq('Eventos não podem ser criados no passado')
-  end
+      expect(event.save).to be false
+      expect(event.errors.messages[:in_the_past].first).to eq('Eventos não podem ser criados no passado')
+    end
 
-  it 'Should not create events in the weekends' do
-    first_saturday_in_the_year = Date.commercial(2022, 1, 6)
-    event = build_event(first_saturday_in_the_year, first_saturday_in_the_year + 1.hour)
+    it 'Should not create events in the weekends' do
+      first_saturday_in_the_year = Date.commercial(2022, 1, 6)
+      event = build_event(first_saturday_in_the_year, first_saturday_in_the_year + 1.hour)
 
-    expect(event.save).to be false
-    expect(event.errors.messages[:weekend].first).to eq('Eventos não podem ser criados aos finais de semana')
-  end
+      expect(event.save).to be false
+      expect(event.errors.messages[:weekend].first).to eq('Eventos não podem ser criados aos finais de semana')
+    end
 
-  it 'Should not create events between days' do
-    now = Time.current
-    start_at = now + 1.day
-    end_at = now + 2.days
+    it 'Should not create events between days' do
+      now = Time.current
+      start_at = now + 1.day
+      end_at = now + 2.days
 
-    event = build_event(start_at, end_at)
+      event = build_event(start_at, end_at)
 
-    expect(event.save).to be false
-    expect(event.errors.messages[:same_day].first).to eq('As datas de início e fim do evento estão em dias diferentes')
-  end
+      expect(event.save).to be false
+      expect(event.errors.messages[:same_day].first).to eq('As datas de início e fim do evento estão em dias diferentes')
+    end
 
-  it 'Should not create events outside of business hours' do
-    start_at = Time.zone.parse('05-05-2022 17:30:00')
-    end_at = start_at + 2.hours
+    it 'Should not create events outside of business hours' do
+      start_at = Time.zone.parse('05-05-2022 17:30:00')
+      end_at = start_at + 2.hours
 
-    event = build_event(start_at, end_at)
+      event = build_event(start_at, end_at)
 
-    expect(event.save).to be false
-    expect(event.errors.messages[:business_hours].first).to eq('O horário do evento está fora do horário comercial')
-  end
+      expect(event.save).to be false
+      expect(event.errors.messages[:business_hours].first).to eq('O horário do evento está fora do horário comercial')
+    end
 
-  it 'Should not create nested events' do
-    start_at = 4.days.from_now
-    end_at = start_at + 2.hours
+    it 'Should not create nested events' do
+      start_at = 4.days.from_now
+      end_at = start_at + 2.hours
 
-    event1 = build_event(start_at, end_at)
-    event1.save!
+      event1 = build_event(start_at, end_at)
+      event1.save!
 
-    event = build_event(start_at + 10.minutes, start_at + 1.hour)
+      event = build_event(start_at + 10.minutes, start_at + 1.hour)
 
-    expect(event.save).to be false
-    expect(event.errors.messages[:booking_conflict].first).to eq('Já existe um evento na sala/horário selecionados')
-  end
+      expect(event.save).to be false
+      expect(event.errors.messages[:booking_conflict].first).to eq('Já existe um evento na sala/horário selecionados')
+    end
 
-  it 'Should not create overlapping events' do
-    start_at = 4.days.from_now
-    end_at = start_at + 2.hours
+    it 'Should not create overlapping events' do
+      start_at = 4.days.from_now
+      end_at = start_at + 2.hours
 
-    event1 = build_event(start_at, end_at)
-    event1.save!
+      event1 = build_event(start_at, end_at)
+      event1.save!
 
-    event = build_event(start_at + 1.hour, start_at + 3.hours)
+      event = build_event(start_at + 1.hour, start_at + 3.hours)
 
-    expect(event.save).to be false
-    expect(event.errors.messages[:booking_conflict].first).to eq('Já existe um evento na sala/horário selecionados')
+      expect(event.save).to be false
+      expect(event.errors.messages[:booking_conflict].first).to eq('Já existe um evento na sala/horário selecionados')
+    end
+
+    it 'Should not create events when start_at is after end_at' do
+      start_at = 4.days.from_now
+      end_at = start_at - 2.hours
+
+      event = build_event(start_at, end_at)
+
+      expect(event.save).to be false
+      expect(event.errors.messages[:end_at].first).to eq('A data de término do evento deve ser maior que a data de início')
+    end
   end
 
   it 'Should create an event' do
