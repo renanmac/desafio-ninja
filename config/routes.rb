f770e11ed0a1f65d31b5103d0a7cddc3084d3de4
Renan Machado <renanmacs@gmail.com>
2022-01-22 02:26:22 -0300

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,10 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  namespace :v1 do
+    resources :schedules, only: :index do
+      resources :events
+      resources :rooms
+    end
+  end
 end
